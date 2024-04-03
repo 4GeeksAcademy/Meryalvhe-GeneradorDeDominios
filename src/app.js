2aea7378cf2a2a0def9990232e86bf742035cda1
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-04-03 15:06:13 +0000

    feat:lineamientos de codigo

--- src/app.js
+++ src/app.js
@@ -6,25 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let ext = [".com", ".co", ".es"];
-
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domain = [".com", ".co", ".es"];
+  // Generador de dominios con for
   let textFor = '<ul class="list-group">';
   textFor +=
     '<li class="list-group-item bg-success text-light"> DOMINIOS FOR </li>';
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let r = 0; r < ext.length; r++) {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let r = 0; r < domain.length; r++) {
           textFor +=
             '<li class="list-group-item">' +
-            pronoun[i] +
-            adj[j] +
-            noun[k] +
-            ext[r] +
+            pronouns[i] +
+            adjectives[j] +
+            nouns[k] +
+            domain[r] +
             "</li>";
         }
       }
@@ -33,15 +33,15 @@ window.onload = function() {
 
   textFor += "</ul>";
   document.querySelector("#dominios").innerHTML = textFor;
-
+  // Generador de dominios con for...of
   let textForOf = '<ul class="list-group">';
   textForOf +=
     '<li class="list-group-item bg-success text-light"> DOMINIOS FOR OF </li>';
 
-  for (const element of pronoun) {
-    for (const item of adj) {
-      for (const arr of noun) {
-        for (const otros of ext) {
+  for (const element of pronouns) {
+    for (const item of adjectives) {
+      for (const arr of nouns) {
+        for (const otros of domain) {
           textForOf += `<li class="list-group-item">
             ${element}${item}${arr}${otros}
             </li>`;
@@ -52,14 +52,14 @@ window.onload = function() {
 
   textForOf += "</ul>";
   document.querySelector("#for").innerHTML = textForOf;
-
+  // Generador de dominios con map
   let textMap = "<ul class='list-group'>";
   textMap +=
     "<li class='list-group-item bg-success text-light'> DOMINIOS MAP</li>";
-  pronoun.map(item => {
-    adj.map(element => {
-      noun.map(itern => {
-        ext.map(elem => {
+  pronouns.map(item => {
+    adjectives.map(element => {
+      nouns.map(itern => {
+        domain.map(elem => {
           textMap += `<li class="list-group-item">${item}${element}${itern}${elem}</li>`;
         });
       });
